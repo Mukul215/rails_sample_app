7427677203758be03255cf0028181bdf43eac774
Mukul B. Patel <Mukul215@gmail.com>
2014-06-22 16:12:49 -0400

    Added a site layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
   def home
+  	# This will show the title to the associated pages
+  	@title = "Home"
   end
 
   def contact
+  	@title = "Contact"
+  end
+
+  def about
+  	@title = "About"
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe PagesController, :type => :controller do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       expect(response).to be_success
+    end
+
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+      # www.rubular.com has a place to make expressions to test
+      # since layout is used for all pages, we only have to use this test for one page
     end
   end
 
@@ -16,4 +25,10 @@ RSpec.describe PagesController, :type => :controller do
     end
   end
 
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      expect(response).to be_success
+    end
+  end
 end
