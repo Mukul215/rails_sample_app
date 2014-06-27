9788f06b587a826a4d5da8f4842e262c9d67f05c
Mukul B. Patel <Mukul215@gmail.com>
2014-06-27 00:46:27 -0400

    Added a form and authentication for sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name
   end
+
+  def create
+    # we do not use this next line in development
+    # raise params[:user].inspect
+    @user = User.new(params[:id])
+    
+    if @user.save
+      redirect_to @user, :flash => { :success => "Welcome to the Sample App!"}
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
